e48478b12a6deeece4c788ff235296ab487254df
ajit <ajitmane1810@gmail.com>
2024-11-13 18:10:52 +0530

    test

--- test_backend/lib/bookDetails.dart
+++ test_backend/lib/bookDetails.dart
@@ -14,29 +14,14 @@ class BookDetailPage extends StatelessWidget {
         child: Column(
           children: [
             Image.network(book['imgUrl'], height: 250, fit: BoxFit.cover),
-            SizedBox(height: 16),
-            Text('Author: ${book['author']}', style: TextStyle(fontSize: 18)),
-            Text('Genre: ${book['genre']}', style: TextStyle(fontSize: 18)),
-            Text('Publish Year: ${book['publishYear']}', style: TextStyle(fontSize: 18)),
-            SizedBox(height: 20),
-            Text(
+            const SizedBox(height: 16),
+            Text('Author: ${book['author']}', style: const TextStyle(fontSize: 18)),
+            Text('Genre: ${book['genre']}', style: const TextStyle(fontSize: 18)),
+            Text('Publish Year: ${book['publishYear']}', style: const TextStyle(fontSize: 18)),
+            const SizedBox(height: 20),
+            const Text(
               'Description: \n(Here you can add a description or any other details about the book)',
               style: TextStyle(fontSize: 16),
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-              // 
             ),
           ],
         ),
